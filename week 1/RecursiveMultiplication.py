9680c4585f7da092f21b23491f90ac727e0e771e
Michael Clausen <michaeljclausen@gmail.com>
2018-08-06 15:09:06 -0700

    fixes variable names for Pyhton conventions

--- week 1/RecursiveMultiplication.py
+++ week 1/RecursiveMultiplication.py
@@ -1,30 +1,30 @@
 import math
 import sys
 
-def RecursiveMultiplication (x, y):
-  lenX = len(str(x))
-  lenY = len(str(y))
-  print("x: ", x, "y: ", y, "lenX:", lenX, "lenY: ", lenY)
-  if lenX == 1 and lenY == 1:
+def recursive_multiplication (x, y):
+  len_x = len(str(x))
+  len_y = len(str(y))
+  print("x: ", x, "y: ", y, "len_x:", len_x, "len_y: ", len_y)
+  if len_x == 1 and len_y == 1:
     return int(x)*int(y)
   else:  
-    midX = int(math.floor(lenX/2))
-    midY = int(math.floor(lenY/2))
+    midX = int(math.floor(len_x/2))
+    midY = int(math.floor(len_y/2))
     x = str(x)
     y = str(y)
     a = x[0:midX]
     b = x[midX:]
     c = y[0:midY]
     d = y[midY:]
-    ac = RecursiveMultiplication(str(a), str(c))
-    ad = RecursiveMultiplication(str(a), str(d))
-    bc = RecursiveMultiplication(str(b), str(c))
-    bd = RecursiveMultiplication(str(b), str(d))
-    ans = 10**lenX * ac + 10**(lenX/2) * (ad + bc) + bd
-    print(ans)
+    ac = recursive_multiplication(str(a), str(c))
+    ad = recursive_multiplication(str(a), str(d))
+    bc = recursive_multiplication(str(b), str(c))
+    bd = recursive_multiplication(str(b), str(d))
+    ans = 10**len_x * ac + 10**(len_x/2) * (ad + bc) + bd
+    #print(ans)
 
   return ans
 
 
 if __name__ == '__main__':
-    RecursiveMultiplication(sys.argv[1], sys.argv[2])
+    recursive_multiplication(sys.argv[1], sys.argv[2])
